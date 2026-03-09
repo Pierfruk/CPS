a797aebde840bb2418b2999391a0df29e2570ab6
pierfruk <pierfrancesco.nannoni@tin.it>
2026-03-09 10:15:34 +0100

    .

---
+++ js/stars.js
@@ -0,0 +1,104 @@
+﻿const placeRandomStars = function (container) {
+    return new Promise((resolve) => {
+        // Pixel density: 1 star per 5000 pixels
+        const pixelDensity = 5000;
+        const bodyWidth = document.body.clientWidth - 2;
+        const bodyHeight = document.body.clientHeight - 2;
+        const totalPixels = bodyWidth * bodyHeight;
+        const numberOfStars = Math.floor(totalPixels / pixelDensity);
+        for (let i = 0; i < numberOfStars; i++) {
+            const star = document.createElement("div");
+            star.classList.add("star");
+
+            const randomX = Math.floor(Math.random() * bodyWidth);
+            const randomY = Math.floor(Math.random() * bodyHeight);
+
+            star.dataset.percentX = randomX / bodyWidth;
+            star.dataset.percentY = randomY / bodyHeight;
+
+            star.style.left = `${randomX}px`;
+            star.style.top = `${randomY}px`;
+
+            const pulseDuration = 4 + Math.random() * 8;
+            star.style.animation = `pulse ${pulseDuration}s infinite`;
+
+            container.appendChild(star);
+
+            void star.offsetWidth;
+
+            // Fade in the star
+            setTimeout(() => (star.style.opacity = "1"), 10);
+
+        }
+        resolve();
+    });
+}
+
+//function placeRandomStars(container) {
+//    // Pixel density: 1 star per 5000 pixels
+//    const pixelDensity = 5000;
+
+//    const bodyWidth = document.body.clientWidth - 2;
+//    const bodyHeight = document.body.clientHeight - 2;
+
+//    const totalPixels = bodyWidth * bodyHeight;
+
+//    const numberOfStars = Math.floor(totalPixels / pixelDensity);
+
+//    for (let i = 0; i < numberOfStars; i++) {
+//        const star = document.createElement("div");
+//        star.classList.add("star");
+
+//        const randomX = Math.floor(Math.random() * bodyWidth);
+//        const randomY = Math.floor(Math.random() * bodyHeight);
+
+//        star.dataset.percentX = randomX / bodyWidth;
+//        star.dataset.percentY = randomY / bodyHeight;
+
+//        star.style.left = `${randomX}px`;
+//        star.style.top = `${randomY}px`;
+
+//        const pulseDuration = 4 + Math.random() * 8;
+//        star.style.animation = `pulse ${pulseDuration}s infinite`;
+
+//        container.appendChild(star);
+
+//        void star.offsetWidth;
+
+//        // Fade in the star
+//        setTimeout(() => (star.style.opacity = "1"), 10);
+//    }
+//}
+
+//function AdjustStarPositions() {
+//    const bodyWidth = document.body.clientWidth - 2;
+//    const bodyHeight = document.body.clientHeight - 2;
+
+//    const existingStars = document.querySelectorAll(".star");
+//    existingStars.forEach((star) => {
+//      //  star.style.opacity = "0";
+
+//        const adjustedX = star.dataset.percentX * bodyWidth;
+//        const adjustedY = star.dataset.percentY * bodyHeight;
+
+//        star.style.left = `${adjustedX}px`;
+//        star.style.top = `${adjustedY}px`;
+//    });
+//}
+
+const removeExistingStars = function (container) {
+    return new Promise((resolve) => {
+        const existingStars = document.querySelectorAll(".star");
+        existingStars.forEach((star) => {
+            container.removeChild(star);
+        });
+        resolve();
+    });
+}
+
+//async function removeExistingStars(container) {
+//    const existingStars = document.querySelectorAll(".star");
+//    existingStars.forEach((star) => {
+//        container.removeChild(star);
+//    });
+//}
